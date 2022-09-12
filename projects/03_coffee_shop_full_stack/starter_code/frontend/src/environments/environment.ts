627a2c8aeadc19b8d2003d115382e4ec41e3a566
adelelwan24 <adelelwan24@gmail.com>
2022-09-12 03:39:10 +0200

    required

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:3000', // the running FLASK api server url
   auth0: {
-    url: 'dr-doom.eu.auth0.com', // the auth0 domain prefix
+    url: 'dr-doom.eu', // the auth0 domain prefix
     audience: 'coffee_shop_drinks', // the audience set for the auth0 app
     clientId: 'Tx7bHj4hceIev6resSPrjUQUMi48JanS', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
